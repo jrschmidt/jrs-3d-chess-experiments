605bebbd751a0cb09338752750152af74b186642
jrschmidt <jrschmidt56@gmail.com>
2026-09-16 13:23:29 -0700

    add black and white king and queen icons

--- app.js
+++ app.js
@@ -253,17 +253,68 @@ const rhombusCorners = (half) => {
 const ICON_K_W = `
   <g>
     <symbol id="x-icon-k-w" viewBox="-20 -20 40 40">
-      <circle cx="0" cy="0" r="19" fill="#cccccc" stroke="#000000" stroke-width="2" />
+      <circle cx="0" cy="0" r="19" fill="#6f6f60" stroke="#000000" stroke-width="2" />
     </symbol>
     <use href="#x-icon-k-w" x="-20" y="-20" width="40" height="40" />
     <symbol id="x-icon-k-w-overlay" viewBox="0 0 100 100">
       <polygon
         points="35,5 65,5 65,35 95,35 95,65 65,65 65,80 90,80 80,90 60,100
           40,100 20,90 10,80 35,80 35,65 5,65 5,35 35,35"
-        fill="#333333"
+        fill="#ffffff"
       />
     </symbol>
     <use href="#x-icon-k-w-overlay" x="-19" y="-19" width="38" height="38" />
+  </g>
+`;
+
+const ICON_K_B = `
+  <g>
+    <symbol id="x-icon-k-b" viewBox="-20 -20 40 40">
+      <circle cx="0" cy="0" r="19" fill="#6f6f60" stroke="#000000" stroke-width="2" />
+    </symbol>
+    <use href="#x-icon-k-b" x="-20" y="-20" width="40" height="40" />
+    <symbol id="x-icon-k-b-overlay" viewBox="0 0 100 100">
+      <polygon
+        points="35,5 65,5 65,35 95,35 95,65 65,65 65,80 90,80 80,90 60,100
+          40,100 20,90 10,80 35,80 35,65 5,65 5,35 35,35"
+        fill="#000000"
+      />
+    </symbol>
+    <use href="#x-icon-k-b-overlay" x="-19" y="-19" width="38" height="38" />
+  </g>
+`;
+
+const ICON_Q_W = `
+  <g>
+    <symbol id="x-icon-q-w" viewBox="-20 -20 40 40">
+      <circle cx="0" cy="0" r="19" fill="#6f6f60" stroke="#000000" stroke-width="2" />
+    </symbol>
+    <use href="#x-icon-q-w" x="-20" y="-20" width="40" height="40" />
+    <symbol id="x-icon-q-w-overlay" viewBox="0 0 100 100">
+      <polygon
+        points="50,30 73,8 67,37 97,33 75,55 75,70 90,80 80,90 60,100
+          40,100 20,90 10,80 25,70 25,55 3,33 33,37 27,8"
+        fill="#ffffff"
+      />
+    </symbol>
+    <use href="#x-icon-q-w-overlay" x="-19" y="-19" width="38" height="38" />
+  </g>
+`;
+
+const ICON_Q_B = `
+  <g>
+    <symbol id="x-icon-q-b" viewBox="-20 -20 40 40">
+      <circle cx="0" cy="0" r="19" fill="#6f6f60" stroke="#000000" stroke-width="2" />
+    </symbol>
+    <use href="#x-icon-q-b" x="-20" y="-20" width="40" height="40" />
+    <symbol id="x-icon-q-b-overlay" viewBox="0 0 100 100">
+      <polygon
+        points="50,30 73,8 67,37 97,33 75,55 75,70 90,80 80,90 60,100
+          40,100 20,90 10,80 25,70 25,55 3,33 33,37 27,8"
+        fill="#000000"
+      />
+    </symbol>
+    <use href="#x-icon-q-b-overlay" x="-19" y="-19" width="38" height="38" />
   </g>
 `;
 
@@ -671,19 +722,19 @@ const buildScene = () => {
 
 buildScene();
 
-placeIcon(ICON_K_W, 5, 4, 3);
+placeIcon(ICON_K_B, 5, 4, 3);
 placeIcon(ICON_K_W, 5, 6, 4);
-placeIcon(ICON_K_W, 5, 3, 5);
-placeIcon(ICON_K_W, 5, 2, 5);
-placeIcon(ICON_K_W, 5, 8, 5);
+placeIcon(ICON_Q_B, 5, 3, 5);
+placeIcon(ICON_Q_W, 5, 2, 5);
+placeIcon(ICON_K_B, 5, 8, 5);
 placeIcon(ICON_K_W, 5, 8, 3);
-placeIcon(ICON_K_W, 5, 5, 1);
-placeIcon(ICON_K_W, 5, 6, 3);
-placeIcon(ICON_K_W, 5, 5, 5);
+placeIcon(ICON_Q_B, 5, 5, 1);
+placeIcon(ICON_Q_W, 5, 6, 3);
+placeIcon(ICON_K_B, 5, 5, 5);
 placeIcon(ICON_K_W, 5, 6, 5);
-placeIcon(ICON_K_W, 5, 3, 3);
-placeIcon(ICON_K_W, 3, 3, 1);
-placeIcon(ICON_K_W, 2, 1, 4);
+placeIcon(ICON_Q_B, 5, 3, 3);
+placeIcon(ICON_Q_W, 3, 3, 1);
+placeIcon(ICON_K_B, 2, 1, 4);
 placeIcon(ICON_K_W, 1, 6, 1);
-placeIcon(ICON_K_W, 4, 5, 1);
-placeIcon(ICON_K_W, 3, 5, 1);
+placeIcon(ICON_Q_B, 4, 5, 1);
+placeIcon(ICON_Q_W, 3, 5, 1);
